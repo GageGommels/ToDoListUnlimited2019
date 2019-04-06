56f1ae385eb54fb0f0bd196c004809adb00d87ce
Gage Gommels <gommelsgage@gmail.com>
2019-04-05 20:38:02 -0600

    Constructors: Just added the constructors to the Task Class

--- TodoListUnlimited/src/task.java
+++ TodoListUnlimited/src/task.java
@@ -1,4 +1,29 @@
 
 public class task {
+	
+	private String name;
+	private int status;
+	private String description;
+	
+	public task() {
+		String name;
+		int status;
+		String description;
+	}
+	
+	public task(String name_Input,int status_Input,String description_Input) {
+		name = name_Input;
+		status = status_Input;
+		description = description_Input;
+	}
 
+	
+	public void set_Name(String name_Input) {
+		
+	}
+	
+	public String get_Name() {
+		return name;
+	}
+	
 }
