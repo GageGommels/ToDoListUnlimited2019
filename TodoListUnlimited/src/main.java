3a89d2435bc1e0c418063ecabdccb110f65f96c3
Gage Gommels <gommelsgage@gmail.com>
2019-04-09 01:12:25 -0600

    Update:
    I added all getters and setters for the Task class
    I am also messing around in main(not permainate but know we can store a
    task object in an array list)

--- TodoListUnlimited/src/main.java
+++ TodoListUnlimited/src/main.java
@@ -1,9 +1,45 @@
+import java.io.*;
+import java.util.*;
 
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int input = 0;
 
+		Scanner scan = new Scanner(System.in);
+
+		ArrayList<task> tList = new ArrayList<task>();
+
+		while (true) {
+
+			printMenu();
+			
+			input = scan.nextInt();
+
+			switch (input) {
+			case 1:
+				task nTask = new task();
+				nTask.populateTask();
+				tList.add(nTask);
+				System.out.println("Task was added!");
+				break;
+			case 2:
+				System.out.println(tList.get(0).toString());
+				break;
+			case 3:
+				System.exit(0);
+				break;
+			default:
+				System.out.print("Please enter a valid input!");
+				break;
+			}
+		}
 	}
-
+	
+	
+	public static void printMenu() {
+		System.out.printf("1: Add Task\n"
+						+ "2: Print List\n"
+						+ "3: Exit Program\n");
+	}
 }
--- TodoListUnlimited/src/task.java
+++ TodoListUnlimited/src/task.java
@@ -1,6 +1,7 @@
+import java.util.*;
 
 public class task {
-	
+	Scanner in = new Scanner(System.in);
 	private String name;
 	private int status;
 	private String description;
@@ -11,7 +12,7 @@ public class task {
 		String description;
 	}
 	
-	public task(String name_Input,int status_Input,String description_Input) {
+	public task(String name_Input, int status_Input, String description_Input) {
 		name = name_Input;
 		status = status_Input;
 		description = description_Input;
@@ -19,11 +20,44 @@ public class task {
 
 	
 	public void set_Name(String name_Input) {
-		
+		name = name_Input;
 	}
 	
 	public String get_Name() {
 		return name;
 	}
 	
+	public int get_Status() {
+		return status;
+	}
+	public void set_Status(int input) {
+		status = input;
+	}
+	
+	public void set_Description(String description_Input) {
+		description = description_Input;
+	}
+	
+	public String get_Description() {
+		return description;
+	}
+	
+	public String toString() {
+		String output = "";
+		
+		output =  "Name:\t" + name + "\n"
+				+ "Status:\t" + status + "\n"
+				+ "Description:\t" + description + "\n";
+		return output;
+	}
+	
+	public void populateTask() {
+		System.out.print("Name of Task:\t");
+		name = in.nextLine();
+		System.out.println("");
+		System.out.print("Description:\t");
+		description = in.nextLine();
+		status = 1;
+		
+	}
 }
